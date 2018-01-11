9e05279d9b6bb8a97a4095f79096dc10f0e14681
Loree Nichols <loree.d.nichols@gmail.com>
2018-01-11 22:22:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
 }
-
+describe('add2(n)', function() {
+  it('adds two to n', function(){
+    const n = Math.floor(Math.random() * 1000)
+    expect(window.add2(n)).toEqual(n + 2)
+  })
+})
 function add2(n) {
 
   var two = 2
